1ab460a1a4ac565779710d7cf18c5718da657a1c
Vadim Dalecky <streamich@gmail.com>
2023-06-11 02:03:29 +0200

    refactor: 💡 use auto keyword

--- addon.cc
+++ addon.cc
@@ -1,10 +1,10 @@
 #include <napi.h>
 
 Napi::String ReadUtf8(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::Uint8Array buf = info[0].As<Napi::Uint8Array>();
-  int32_t offset = info[1].As<Napi::Number>().Int32Value();
-  int32_t length = info[2].As<Napi::Number>().Int32Value();
+  auto env = info.Env();
+  auto buf = info[0].As<Napi::Uint8Array>();
+  auto offset = info[1].As<Napi::Number>().Int32Value();
+  auto length = info[2].As<Napi::Number>().Int32Value();
   auto str = Napi::String::New(env, (char*)buf.Data() + offset, length);
   return str;
 }
